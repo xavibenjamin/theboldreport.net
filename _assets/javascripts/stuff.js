1cb8c23dc6928a84c53433c9f2fc7e4aab302f18
Tim Smith <smith@ttimsmith.com>
2014-10-21 16:42:52 -0500

    Finishing touches
    - New navigation
    - Adding search form to search page

--- _assets/javascripts/stuff.js
+++ _assets/javascripts/stuff.js
@@ -5,3 +5,7 @@ $(".video-pusher").fitVids();
 $(".toggleNav").on("click", function() {
     $(".mobile-nav").toggleClass("MenuIsOpen");
 });
+
+$(".toggleTopBar").on("click", function() {
+  $("body").toggleClass("BarShow");
+})
