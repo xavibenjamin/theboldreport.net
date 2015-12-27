442193a684c6f13ccda29a7a245efcce040ec1b2
Tim Smith <smith@ttimsmith.com>
2015-12-27 17:47:40 -0600

    switching to rouge

--- _assets/javascripts/stuff.js
+++ _assets/javascripts/stuff.js
@@ -1,11 +1,3 @@
 //= require vendor/jquery.fitvids
 
 $(".video-pusher").fitVids();
-
-$(".toggleNav").on("click", function() {
-    $(".mobile-nav").toggleClass("MenuIsOpen");
-});
-
-$(".toggleTopBar").on("click", function() {
-  $("body").toggleClass("BarShow");
-})
