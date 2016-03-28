4835460adb8c93a59e23c37322ec106c4615d8a6
Tim Smith <smith@ttimsmith.com>
2016-03-27 22:13:09 -0500

    Redesigning search

--- _assets/javascripts/search.js
+++ _assets/javascripts/search.js
@@ -10,3 +10,22 @@ $(function() {
     template: '#search-results-template'
   });
 });
+
+$(document).ready(function() {
+  var searchButton = $('.js-search-toggle');
+  var searchWrapper = $('.site__search-form');
+  var searchInput = $('#search-query');
+  var isOpen = false;
+  searchButton.click(function() {
+    if(isOpen == false) {
+      searchWrapper.addClass('js-search-open');
+      searchInput.focus();
+      isOpen = true;
+    }
+    else {
+      searchWrapper.removeClass('js-search-open');
+      searchInput.focusout();
+      isOpen = false;
+    }
+  });
+});
