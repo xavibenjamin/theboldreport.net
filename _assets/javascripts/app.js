910282d9d900b705d2c3fb25f65f613d39a5f1df
Tim Smith <smith@ttimsmith.com>
2016-08-12 23:33:01 -0500

    add compress and fix things so that it works (#101)

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -1,3 +1,8 @@
 //= require ./vendor/jquery.min
 //= require newsletter
 //= require search
+//= require bigfoot.min
+
+jQuery(document).ready(function($) {
+  $.bigfoot();
+});
--- _assets/javascripts/fittext.js
+++ _assets/javascripts/fittext.js
@@ -13,7 +13,6 @@
 
   $.fn.fitText = function( kompressor, options ) {
 
-    // Setup options
     var compressor = kompressor || 1,
         settings = $.extend({
           'minFontSize' : Number.NEGATIVE_INFINITY,
@@ -22,18 +21,14 @@
 
     return this.each(function(){
 
-      // Store the object
       var $this = $(this);
 
-      // Resizer() resizes items based on the object width divided by the compressor * 10
       var resizer = function () {
         $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
       };
 
-      // Call once to set.
       resizer();
 
-      // Call on resize. Opera debounces their resize by default.
       $(window).on('resize.fittext orientationchange.fittext', resizer);
 
     });
