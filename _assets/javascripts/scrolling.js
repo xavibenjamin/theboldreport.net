1de92e6e22aca2a776f96b8a24eac926fa409dce
Tim Smith <smith@ttimsmith.com>
2016-11-15 15:37:14 -0600

    Fiddle with the numbers till it's just right. Bug shows only when it should

--- _assets/javascripts/scrolling.js
+++ _assets/javascripts/scrolling.js
@@ -2,16 +2,17 @@ var $body = $('body'),
     $scroll_class = 'scrolled',
     $header_height = $('.site__header').height(),
     $main_height = $('.content__main-content').height(),
+    $footer_height = $('.site__footer').height(),
     $offset_val = $header_height + $main_height;
 
 function stickyBug() {
-  if (window.pageYOffset > 50){
+  if (window.pageYOffset > 1000){
     $body.addClass($scroll_class);
   } else {
     $body.removeClass($scroll_class);
   }
 
-  if (window.pageYOffset > $offset_val - 300) {
+  if (window.pageYOffset > $offset_val - 700) {
     $body.removeClass($scroll_class);
   }
 
